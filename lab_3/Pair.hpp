e524285184d5072f85a274e77035d050b433dc83
Яна <yanaakipova@gmail.com>
2026-04-16 22:42:06 +0300

    A pair of objects

---
+++ lab_3/Pair.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+
+template<typename T, typename U>
+
+class Pair{
+private:
+    T first;
+    U second;
+public:
+    Pair(const T& one, const U& two): first(one), second(two){}
+
+    T GetFirst() const{
+        return first;
+    }
+    U GetSecond() const{
+        return second;
+    }
+
+    bool operator==(const Pair& other){
+        return (other.first == first && other.second == second);
+    }
+
+    bool operator!=(const Pair& other){
+        return !5(other == *this);
+    }
+
+    std::string ToString() const{
+        std::ostringstream oss;
+        oss << "(" first << " " << second << ")";
+
+        return oss.str();
+    }
+};
